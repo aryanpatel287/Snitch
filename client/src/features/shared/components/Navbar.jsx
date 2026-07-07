eebc57ab52fc56fd4edb4143491ef966c81f8138
aryanpatel287 <aryanpatel.me@gmail.com>
2026-07-07 22:14:57 +0530

    feat: remove subnavbar markup from Navbar component

--- client/src/features/shared/components/Navbar.jsx
+++ client/src/features/shared/components/Navbar.jsx
@@ -144,33 +144,6 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                {/* Bottom Row: Scrollable Clothes Categories */}
-                <div className="navbar__categories-scroll-bar">
-                    <div className="navbar__scroll-container navbar__desktop-only-categories">
-                        {categories.map((cat) => (
-                            <Link 
-                                key={cat.name} 
-                                to={cat.path} 
-                                className="navbar__category-tab"
-                                onClick={closeAllMenus}
-                            >
-                                {cat.name}
-                            </Link>
-                        ))}
-                    </div>
-                    <div className="navbar__mobile-search-container">
-                        <form className="navbar__search-box navbar__search-box--mobile" onSubmit={handleSearchSubmit}>
-                            <i className="ri-search-line navbar__search-icon"></i>
-                            <input
-                                type="text"
-                                placeholder='Search "STRAIGHT FIT JEANS"'
-                                className="navbar__search-field"
-                                value={searchVal}
-                                onChange={(e) => setSearchVal(e.target.value)}
-                            />
-                        </form>
-                    </div>
-                </div>
             </nav>
 
             {/* Slide Drawer Category Menu (Left-sliding) */}
