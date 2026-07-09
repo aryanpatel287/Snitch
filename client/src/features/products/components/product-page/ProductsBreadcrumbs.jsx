7d5d1d0c177f308f1b6a7ec2f5e0b255ecc22e14
aryanpatel287 <aryanpatel.me@gmail.com>
2026-07-10 01:26:54 +0530

    feat: extract ProductsBreadcrumbs component and split its stylesheet

---
+++ client/src/features/products/components/product-page/ProductsBreadcrumbs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+import '../../styles/_products-breadcrumbs.scss';
+
+const ProductsBreadcrumbs = () => {
+    return (
+        <nav className="products-breadcrumbs" aria-label="breadcrumb">
+            <Link to="/" className="products-breadcrumbs__link">
+                Home
+            </Link>
+            <i className="ri-arrow-right-s-line products-breadcrumbs__sep"></i>
+            <span className="products-breadcrumbs__current">Shop</span>
+        </nav>
+    );
+};
+
+export default ProductsBreadcrumbs;
